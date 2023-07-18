f145f673ff25b15eb092d7ab2e5231528f6c5de9
abdulsalam alkhateeb <abdulsalam@matched.se>
2023-07-18 06:56:26 +0300

    first commit

--- config/constant.php
+++ config/constant.php
@@ -3,7 +3,7 @@
 //ON or OFF
 return
     [
-    'thermal_printer' => 'OFF',
-    'currency' => 'Rs',
-    'copyright' => 'Copyright © 2023 Medix v4.0. All rights reserved.',
+    'thermal_printer' => 'ON',
+    'currency' => 'AED',
+    'copyright' => 'Copyright © 2023 Alhayat Pharmacy. All rights reserved.',
 ];
