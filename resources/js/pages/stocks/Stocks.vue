81dc2ae6fb171ae5ee121bdc0c2ebcbb56d1f2c0
khaledDo2 <57242582+khaledDo2@users.noreply.github.com>
2023-08-10 13:35:31 +0300

    update barcode font

--- resources/js/pages/stocks/Stocks.vue
+++ resources/js/pages/stocks/Stocks.vue
@@ -1110,17 +1110,17 @@ export default class Stocks extends Vue {
         const htmlContent = `
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,800;1,800&display=swap" rel="stylesheet">
             <div style="text-align: left;">
-                <div  style="font-size: 19px; font-family: cursive, 'Poppins' ;font-weight: bolder;">${
+                <div  style="font-size: 19px; font-family: Arial, 'Poppins' ;font-weight: 500;">${
                     words[0]
                 }</div>
-                <div style="font-size: 21px ; font-family: cursive, 'Poppins' ;font-weight: bolder;">${
+                <div style="font-size: 21px ; font-family: Arial, 'Poppins' ;font-weight: 500;">${
                     words[1]
                 }</div>
                 <img src="${canvasElement.toDataURL()}" />
 
                 <div style="display: flex; justify-content: space-between; align-items: center; padding: 0 10px;">
-                    <div style="font-size: 21px; font-family: cursive, 'Poppins' ; font-weight: bolder;">${words[3]}</div>
-                    <div style="font-size: 21px; font-family: cursive, 'Poppins' ; font-weight: bolder;">EXP: ${expiryDate}</div>
+                    <div style="font-size: 21px; font-family: Arial, 'Poppins' ; font-weight: 500;">${words[3]}</div>
+                    <div style="font-size: 21px; font-family: Arial, 'Poppins' ; font-weight: 500;">EXP: ${expiryDate}</div>
                 </div>
             </div>
             `;
